1630413b578077b70e1f0cde9012e07145733309
arthurfrancischini025 <arthur.francischini@usp.br>
2025-05-20 12:42:12 -0400

    ciclo 2.2

---
+++ treinamento_2/vetor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int numeros[10];
+    int i;
+    int j;
+    int pares = 0;
+    int impares = 0;
+
+    for (i=0;i<10;i++){
+        printf("Digite um número:");
+        scanf("%d", &numeros[i]);
+    }
+    for(j=0;j<10;j++){
+        if (numeros[j] % 2 == 0){
+            pares++;
+        }
+        else {
+            impares++;
+        }
+    }
+
+    printf("Qauntidade de números pares: %d\n", pares);
+    printf("Quantidade de números ímpares: %d", impares);
+}
